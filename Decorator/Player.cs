7be01b159b455c25ae8ce893fcf0f0a5f3b0cb92
Patryk Cislo <patrykcislo@gmail.com>
2017-03-27 20:04:01 +0200

    Decorator was added

---
+++ Decorator/Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class Component
+    {
+        protected Component BaseComponent;
+        public abstract int ComputePower();
+    }
+
+    public class WeaponComponent : Component
+    {
+        private const int ComponentPower = 5;
+
+        public WeaponComponent(Component component)
+        {
+            BaseComponent = component;
+        }
+        public override int ComputePower()
+        {
+            return BaseComponent.ComputePower() + ComponentPower;
+        }
+        public override string ToString()
+        {
+            return BaseComponent.ToString() + " with weapon";
+        }
+    }
+
+    public class ShieldComponent : Component
+    {
+        private const int ComponentPower = 3;
+
+        public ShieldComponent(Component component)
+        {
+            BaseComponent = component;
+        }
+        public override int ComputePower()
+        {
+            return BaseComponent.ComputePower() + ComponentPower;
+        }
+        public override string ToString()
+        {
+            return BaseComponent.ToString() + " with shield";
+        }
+    }
+
+    public class Player : Component
+    {
+        public override int ComputePower()
+        {
+            return 0;
+        }
+        public override string ToString()
+        {
+            return "base player";
+        }
+    }
+}
--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -10,6 +10,17 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+            Console.WriteLine($"Player: {player}");
+
+            WeaponComponent playerWithWeapon = new WeaponComponent(player);
+            Console.WriteLine($"Player with weapon: {playerWithWeapon}");
+
+            ShieldComponent playerWithShield = new ShieldComponent(player);
+            Console.WriteLine($"Player with shield: {playerWithShield}");
+
+            ShieldComponent playerWithWeaponAndShield = new ShieldComponent(playerWithWeapon);
+            Console.WriteLine($"Player with weapon and shield: {playerWithWeaponAndShield}");
         }
     }
 }
