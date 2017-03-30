23f3362d6209d63b74aa9e29bb2914882e264d4b
Patryk Cislo <patrykcislo@gmail.com>
2017-03-30 14:02:29 +0200

    Add Singleton

---
+++ Singleton/GraphicManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class GraphicManager
+    {
+        private static GraphicManager _instance;
+        private static object lockObject = new object();
+
+        public int Id { get; }
+        public ConsoleColor Color { get; set; }
+        public static GraphicManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if(_instance == null)
+                            _instance = new GraphicManager();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private GraphicManager()
+        {
+            Random rnd = new Random();
+            Id = rnd.Next(1_000_000);
+        }
+
+        public void Render(string msg)
+        {
+            Console.ForegroundColor = Color;
+            Console.WriteLine($"[GRAPHIC ID: {Id}] RENDERS: {msg}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphicManager.Instance.Color = ConsoleColor.Cyan;
+            GraphicManager.Instance.Render("Hello World in Cyan");
+
+            GraphicManager.Instance.Color = ConsoleColor.Yellow;
+            GraphicManager.Instance.Render("Second message");
+        }
+
+    }
+}
