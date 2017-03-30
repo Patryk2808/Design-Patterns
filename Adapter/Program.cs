09c3fc06cb19ea6856a05ed27a862c0b29bac496
Patryk Cislo <patrykcislo@gmail.com>
2017-03-30 19:16:42 +0200

    Add Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            Product p1 = new Product() {Id = 1002, Name = "Ketchup", Price = 3.2};
+            Product p2 = new Product() { Id = 1008, Name = "Yoghurt", Price = 4.99 };
+
+            shop.AddProduct(new ProductAdapter(p1));
+            shop.AddProduct(new ProductAdapter(p2));
+
+            shop.PrintAll();
+
+
+        }
+    }
+}
---
+++ Adapter/Shop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public interface IProduct
+    {
+        void Print();
+    }
+
+    public class ProductAdapter : IProduct
+    {
+        private Product _product;
+
+        public ProductAdapter(Product p)
+        {
+            _product = p;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"PRODUCT: id: {_product.Id} name: {_product.Name} price: {_product.Price}");
+        }
+    }
+
+    public class Shop
+    {
+        private List<IProduct> _products = new List<IProduct>();
+
+        public void AddProduct(IProduct p)
+        {
+            _products.Add(p);
+        }
+
+        public void PrintAll()
+        {
+            foreach (var product in _products)
+            {
+                product.Print();
+            }
+        }
+    }
+}
